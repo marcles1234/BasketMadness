e8a0867ee21068f7e25d1bae7f06e7497d1b7f7b
Marc Jones <marc.ioan55@gmail.com>
2025-02-05 15:17:53 +0000

    basketbackgound2

--- game.js
+++ game.js
@@ -2,13 +2,13 @@
 class Example extends Phaser.Scene {
     preload() {
         this.load.setBaseURL('https://labs.phaser.io');
-        this.load.image('sky', 'https://raw.githubusercontent.com/marcles1234/BasketMadness/refs/heads/main/assets/origins.jpg');
+        this.load.image('sky', 'https://raw.githubusercontent.com/marcles1234/BasketMadness/refs/heads/main/assets/BasketCourt.png');
         this.load.image('logo', 'assets/sprites/phaser3-logo.png');
         this.load.image('red', 'assets/particles/red.png');
     }
 
     create() {
-        this.add.image(400, 300, 'sky');
+        this.add.image(600, 300, 'sky');
 
         const particles = this.add.particles(0, 0, 'red', {
             speed: 100,
